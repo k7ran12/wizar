c2324a9033c6d1fa2fb471b1376049f1b9bc7e04
Cristian AV <tauro24_vs@hotmail.com>
2023-01-29 23:27:55 -0500

    Todos los archivos

---
+++ resources/views/documento/contratoDeCompraVenta.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container {
+            margin: 0 20px;
+        }
+
+        p {
+            text-align: justify;
+        }
+
+        p.primeralinea {
+            text-indent: 60px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .firma {
+            position: absolute;
+            bottom: 0;
+        }
+
+        h5 {
+            text-align: center;
+        }
+
+        .divdos {
+            margin: 80px 0;
+        }
+
+        .sin_margin {
+            margin: 10px 0;
+        }
+
+        .con_margin {
+            margin: 30px 0;
+        }
+        .firma > div{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            text-align: center;
+        }
+        table{
+            width: 100%;
+        }
+        table tr td{
+            width: 300px;
+            text-align: center;
+            margin: 10px 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>CONTRATO DE COMPRAVENTA</h1>
+        <p>En PARRAL, a 29 de enero de 2023</p>
+        <div>
+            <p class="con_margin">Entre MARCELO TRISTIAM AGUILERA FERNÁNDEZ C.I. N° 11.531.691-5 domiciliado para estos efectos en CALLEJON LOS CEREZOS S/N MAITENES, COMUNA RETIRO, como VENDEDOR Y LUIS EDMONGE GATICA VASQUEZ C.I. N° 10.187.735-3 domiciliado para estos efectos en AVENIDA 2 SUR 63PARRAL, COMUNA  como COMPRADOR se ha convenido el siguiente contrato de compraventa:</p>
+            <p class="con_margin">PRIMERO. - MARCELO TRISTIAM AGUILERA FERNÁNDEZ vende a LUIS EDMONGE GATICA VASQUEZ quien compra y acepta para sí un vehículo Inscripción : UK.1013-8 Tipo Vehículo : CAMION Año : 2001 Marca : CHEVROLET Modelo : NKR 69 Nro. Motor : 727861 Nro. Chasis : JAANKR69LX7100670 Color : BLANCO, con sus accesorios completos que el comprador declara haber recibido a su entera satisfacción, y después de haberlo revisado detenidamente por lo que se expresa no tener cargo alguno ni objeción de formular, declarando que renuncia expresamente a las acciones de saneamiento por vicios redhibitorios establecido en el párrafo 8 del Título XXIII del Libro IV del Código Civil. El vehículo se vende en el estado en que se encuentra y que el comprador conoce cabalmente.</p>
+            <p class="con_margin">SEGUNDO. - El precio de venta es la suma de $2.466.152.- (DOS MILLONES CUATROCIENTOS SESENTA Y SEIS MIL CIENTO CINCUENTA Y DOS PESOS) que el comprador paga de la siguiente forma: $ 2.466.152.- en EFECTIVO. Con una tasación fiscal de $ 2.466.152.- (DOS MILLONES CUATROCIENTOS SESENTA Y SEIS MIL CIENTO CINCUENTA Y DOS PESOS)</p>
+            <p class="con_margin">TERCERO. - Declaran los vendedores bajo juramento que el vehículo que venden no tiene deudas ni gravámenes de ninguna especie: como Prenda, Embargo o Prohibición de enajenar, Los compradores declaran por su parte que este hecho es causa determinante de esta compraventa y que ha estudiado debidamente el dominio vigente del vehículo que compra. Ambas partes liberan a los Notarios que autorizan de toda responsabilidad al respecto.</p>
+            <p class="con_margin">CUARTO. - El comprador queda facultado para requerir por sí solo la anotación de esta transferencia en las oficinas correspondientes.</p>
+            <p class="con_margin">QUINTO. - Los compradores, aceptan el presente vehículo en su situación y responsabilidad de multas, la cual consta en el Certificado de Multas de Tránsito Impagas del Registro Civil.</p>
+            <p class="con_margin">SEXTO. - El presente contrato tiene su validez como documento electrónico de acuerdo con la ley 19.799, el cual ha sido firmado por ambas partes mediante firma electrónica.</p>
+        </div>
+        <div class="firma">
+            <table>
+                <tr>
+                    <td>«NOMBRE_VENDEDOR»</td>
+                    <td>«NOMBRE_REPRESENTANTE_COMPRADOR»</td>
+                </tr>
+                <tr>
+                    <td>C.I. N° «RUT_VENDEDOR»</td>
+                    <td>C.I. N°</td>
+                </tr>
+                <tr>
+                    <td>«RUT_REP_COMPRADOR»</td>
+                    <td>C.P. «RUT_COMPRADOR»</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>_________________________</td>
+                    <td>_________________________</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/documento/contratoRepresentacionVendedor.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container {
+            margin: 0 20px;
+        }
+
+        p {
+            text-align: justify;
+        }
+
+        p.primeralinea {
+            text-indent: 60px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .firma {
+            position: absolute;
+            bottom: 0;
+        }
+
+        h5 {
+            text-align: center;
+        }
+
+        .divdos {
+            margin: 80px 0;
+        }
+
+        .sin_margin {
+            margin: 10px 0;
+        }
+
+        .con_margin {
+            margin: 30px 0;
+        }
+        .firma > div{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            text-align: center;
+        }
+        table{
+            width: 100%;
+        }
+        table tr td{
+            width: 300px;
+            text-align: center;
+            margin: 10px 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>CONTRATO DE COMPRAVENTA</h1>
+        <p>En PARRAL, a 29 de enero de 2023.</p>
+        <div>
+            <p class="sin_margin">Entre TRANSPORTES DEY SPA C.I. N° 77.349.008-2 domiciliado para estos efectos en ARTURO PRAT 2170B, COMUNA CONCEPCION, REGION DE MAULE representado por ESTEBAN ANDRES GOMEZ RAMOS C.I. N° 13.954.979-1 como VENDEDOR Y MANUEL JOSE ESPINAZA MEZA C.I. N° 7.751.677-8 domiciliado para estos efectos en INDEPENDENCIA 720, COMUNA RETIRO como COMPRADOR se ha convenido el siguiente contrato de compraventa:</p>
+            <p class="con_margin">PRIMERO. - TRANSPORTES DEY SPA vende a MANUEL JOSE ESPINAZA MEZA quien compra y acepta para sí un vehículo Inscripción : JK.2374-8 DATOS DEL VEHICULO Tipo Vehículo : SEMIREMOLQUE Año : 2011 Marca : HSV SA Modelo : RPL 12 2011 Nro. Chasis : RPL-4-12-2012 Nro. Serie : RPL-4-12-2012 Color : GRIS Combustible : (NO INFORMADO) PBV : 45.000,00 KILOS, con sus accesorios completos que el comprador declara haber recibido a su entera satisfacción, y después de haberlo revisado detenidamente por lo que se expresa no tener cargo alguno ni objeción de formular, declarando que renuncia expresamente a las acciones de saneamiento por vicios redhibitorios establecido en el párrafo 8 del Título XXIII del Libro IV del Código Civil. El vehículo se vende en el estado en que se encuentra y que el comprador conoce cabalmente.</p>
+            <p class="con_margin">SEGUNDO. - El precio de venta es la suma de $6.000.000.- (SEIS MILLONES DE PESOS) que el comprador paga de la siguiente forma: $ 6.000.000.- en EFECTIVO. Con una tasación fiscal de $ 6.000.000.- (SEIS MILLONES DE PESOS)</p>
+            <p class="con_margin">TERCERO. - Declaran los vendedores bajo juramento que el vehículo que venden no tiene deudas ni gravámenes de ninguna especie: como Prenda, Embargo o Prohibición de enajenar, Los compradores declaran por su parte que este hecho es causa determinante de esta compraventa y que ha estudiado debidamente el dominio vigente del vehículo que compra. Ambas partes liberan a los Notarios que autorizan de toda responsabilidad al respecto.</p>
+            <p class="con_margin">CUARTO. - El comprador queda facultado para requerir por sí solo la anotación de esta transferencia en las oficinas correspondientes.</p>
+            <p class="con_margin">QUINTO. - Los compradores, aceptan el presente vehículo en su situación y responsabilidad de multas, la cual consta en el Certificado de Multas de Tránsito Impagas del Registro Civil.</p>
+            <p class="con_margin">SEXTO. - El vendedor declara expresa y juradamente, en forma individual, que ha firmado el presente instrumento, encontrándose físicamente dentro de los límites territoriales y geográficos de la comuna de PARRAL Región REGION DE MAULE  lo cual le consta fehacientemente por conocer los límites de la comuna referida.</p>
+            <p class="sin_margin">Esta declaración se realiza con el objeto de cumplir con lo dispuesto por el inciso final del artículo 400 del Código Orgánico de Tribunales al momento de la autorización de la firma del presente contrato.</p>
+            <p class="sin_margin">Se deja expresa constancia de que esta declaración ha sido efectuada por el vendedor con el pleno conocimiento respecto de la disposición</p>
+            <p class="sin_margin">contenida en el artículo 210 del Código Penal de la República de Chile, que especifica la responsabilidad por perjurar o dar falso testimonio, asumiendo la responsabilidad legal correspondiente.</p>
+            <p class="con_margin">SÉPTIMO. - El comprador declara expresa y juradamente, en forma individual, que ha firmado el presente instrumento, encontrándose físicamente dentro de los límites territoriales y geográficos de la comuna de PARRAL Región REGION DE MAULE lo cual le consta fehacientemente por conocer los límites de la comuna referida.</p>
+            <p class="sin_margin">Esta declaración se realiza con el objeto de cumplir con lo dispuesto por el inciso final del artículo 400 del Código Orgánico de Tribunales al momento de la autorización de la firma del presente contrato.</p>
+            <p class="sin_margin">Se deja expresa constancia de que esta declaración ha sido efectuada por el comprador con el pleno conocimiento respecto de la</p>
+            <p class="sin_margin">disposición contenida en el artículo 210 del Código Penal de la República de Chile, que especifica la responsabilidad por perjurar o dar falso testimonio, asumiendo la responsabilidad legal correspondiente.</p>
+            <p class="con_margin">OCTAVO. - El presente contrato tiene su validez como documento electrónico de acuerdo con la ley 19.799, el cual ha sido firmado por ambas partes mediante firma electrónica.</p>
+        </div>
+        <div class="firma">
+            <table>
+                <tr>
+                    <td>«NOMBRE_VENDEDOR»</td>
+                    <td>«NOMBRE_REPRESENTANTE_COMPRADOR»</td>
+                </tr>
+                <tr>
+                    <td>C.I. N° «RUT_VENDEDOR»</td>
+                    <td>C.I. N°</td>
+                </tr>
+                <tr>
+                    <td>«RUT_REP_COMPRADOR»</td>
+                    <td>C.P. «RUT_COMPRADOR»</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>_________________________</td>
+                    <td>_________________________</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
